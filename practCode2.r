46433e63b83892eccd0bd53840afa5995f278e24
A Marm Kilpatrick <marmkilpatrick@gmail.com>
2022-01-21 12:45:16 -0800

    adding new r code file

---
+++ practCode2.r
@@ -0,0 +1,3 @@
+m=read.csv("oahu.prevalence.analysis.file.csv") 
+m$Year=as.factor(m$Year);m$Species=factor(m$Species)
+m=m[m$Species%in%c("JAWE","RBLE","RWBU"),] #focus analyses on 3 spp w/ large N
