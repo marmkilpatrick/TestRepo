44fdd00678b43c795bac4e52e82f3cdefff35aec
Billy J. Gardner <wjgardne@ucsc.edu>
2022-01-21 12:50:11 -0800

    Update

--- practCode2.r
+++ practCode2.r
@@ -1,3 +1,5 @@
 m=read.csv("oahu.prevalence.analysis.file.csv") 
 m$Year=as.factor(m$Year);m$Species=factor(m$Species)
 m=m[m$Species%in%c("JAWE","RBLE","RWBU"),] #focus analyses on 3 spp w/ large N
+
+#Happy Squirrel Appreciation Day
